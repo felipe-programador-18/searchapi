3c57a2e5b42e5fa20994123291a35f1274e06372
Felipe <machadofelipe2016@outlook.com>
2022-05-10 15:33:39 -0300

    c0-started new project to practice and learning more about explore api

---
+++ project-api/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Remaking all code to learning how explore api</h1>
+    </div>
+  );
+}
+
+export default App;
