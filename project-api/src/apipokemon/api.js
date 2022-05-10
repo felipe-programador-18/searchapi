4d05ebfad931b0652b2d2e312c296c71528a9583
Felipe <machadofelipe2016@outlook.com>
2022-05-10 17:57:35 -0300

    c2-make way to adding ulr of api date of pokemons

--- project-api/src/apipokemon/api.js
+++ project-api/src/apipokemon/api.js
@@ -1,1 +1,35 @@
 import React from 'react'
+
+//create one functions to manager all date api
+//theoritical this code have manager api
+export const SearchPokemon = async (pokemon) =>{
+    try { 
+       let url = ` https://pokeapi.co/api/v2/pokemon/${pokemon}`
+       const res = await fetch(url)
+       return await res.json()
+                
+    } catch (error) {
+       console.log('error in code', error)
+    }
+}
+
+//create function to get pokemon and boundaries search field to max 50!!
+export const GetPokemons = async (limit=50, offset=0) => {
+   try{
+       let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+       const res = await fetch(url)
+       return await res.json()
+   } catch(error){
+       console.log('error inside search !!')
+   }
+}
+
+//this caught date of pokemon all
+export const GetPokemensData = async (url) => {
+    try {
+       const res = await fetch(url)
+       return await res.json()   
+    } catch (error) {
+       console.log('error in get all date pokemon', error) 
+    }
+}
