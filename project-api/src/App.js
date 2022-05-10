acdf9dcdb759fd4ede1da00a3f55b0bcecdf0b48
Felipe <machadofelipe2016@outlook.com>
2022-05-10 18:33:51 -0300

    c5-adding search bar in page principles

--- project-api/src/App.js
+++ project-api/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './Componentes/Navbar'
+import SearchBar from './Componentes/SearchBar';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <SearchBar/>
     </div>
   );
 }
