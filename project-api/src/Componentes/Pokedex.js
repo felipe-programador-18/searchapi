5b6194b5d6bffb0683e3eb073c0f822134f71b74
Felipe <machadofelipe2016@outlook.com>
2022-05-10 19:42:16 -0300

    c10-create pokedex need practice so much about all that

---
+++ project-api/src/Componentes/Pokedex.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Pokemon from './Pokemon'
+
+const Pokedex = (props) =>{
+  const {pokemons, page, loading, setPage, TotalPage} = props
+
+
+
+  return(
+
+  )
+}
+
+
+export default Pokedex
