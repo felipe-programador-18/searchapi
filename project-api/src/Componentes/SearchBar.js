6b9a1a47e6c9ffe7e68c578ea6bdf6ee083525f5
Felipe <machadofelipe2016@outlook.com>
2022-05-10 18:35:00 -0300

    c7-adding more code

---
+++ project-api/src/Componentes/SearchBar.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+const SearchBar = (props) =>{
+    const[search, setsearch] = useState("dito")
+    const onSearch = props 
+    
+    //inside onhandlingchange i passed e to later get value element!!!
+    const OnHandlingChange = (e) => {
+      setsearch(e.target.value)
+      if(e.target.value === 0){
+        onSearch(undefined)
+      }
+    }
+
+    const OnHandlingButton = () => {
+        onSearch(search)
+    }
+
+ return(
+     <div className='searchbar-container'>
+             <div className='searchbar'>
+              <input placeholder='search-pokemon' onChange={OnHandlingChange} />
+             </div>
+             <div>
+              <button onClick={OnHandlingButton}> </button>
+             </div>
+     </div>)
+}
+
+export default SearchBar
