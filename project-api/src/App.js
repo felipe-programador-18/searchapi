c79f12cac127335152d7a6ca7efb1f17f7fd511c
Felipe <machadofelipe2016@outlook.com>
2022-05-10 19:41:48 -0300

    c9-adding all state and practice more and more about code

--- project-api/src/App.js
+++ project-api/src/App.js
@@ -1,8 +1,21 @@
+import React,{useState} from 'react'
 import './App.css'
+import { SearchPokemon, GetPokemons, GetPokemensData } from './apipokemon/api'; 
 import Navbar from './Componentes/Navbar'
 import SearchBar from './Componentes/SearchBar';
 
 function App() {
+  //flag (page,totalpage, loading, favorites,pokemons and notfound)
+  const [page, setPage] = useState(0)
+  const [TotalPage, setTotalPage] = useState(0)
+  const [loading, setloading]= useState(false)
+  const [notfound, setfound]= useState(false)
+  const [pokemons, setpokemons] = useState([])
+  
+  //this working with context!!
+  const [favorites, setfavorites] = useState([])
+  
+  
   return (
     <div className="App">
      <Navbar/>
