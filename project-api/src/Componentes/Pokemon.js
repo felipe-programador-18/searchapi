ddb55db3ace9d19ac454af9ec16726b0ab399b89
Felipe <machadofelipe2016@outlook.com>
2022-05-11 16:56:36 -0300

    c15-finnaly file about pokemon

--- project-api/src/Componentes/Pokemon.js
+++ project-api/src/Componentes/Pokemon.js
@@ -3,10 +3,17 @@ import FavoriteContext from '../Contexto/FavoritePokemons'
 
 //this page pokemon working together with pokedex!!!
 //traning quite about that!!!
-
+//remember is name (types,name count) i always get api practice about that!
 const Pokemon = (props) =>{
   const {favoritepokemons, updatingpokemons} = useContext(FavoriteContext)
   const {pokemon}= props
+  const heart = favoritepokemons.includes(pokemon.name) ? "💛" : "🖤"
+  
+  const onHanldingPokemon = () =>{
+    updatingpokemons(pokemon.name)
+  }
+
+
   return(
   <div className='pokemon-card'>
     <div className='pokemon-image-container'>
@@ -16,34 +23,22 @@ const Pokemon = (props) =>{
         <div className='card-body'>
            <div className='card-type'> 
                <h3>{pokemon.name}</h3>
-             <div> # {pokemon.id}</div> 
+             <div> #{pokemon.id}</div> 
            </div>
-        
+      
            <div className='card-button'>
                <div className='pokemon-type'>
-
+                {pokemon.types.map((type, index) => 
+                { return <div className='pokemon-type-test' key={index}>
+                     {type.type.name}
+                </div>
+                })}
                </div>
-               <button>
-                   
+               <button className='pokemon-heart-btn' onClick={onHanldingPokemon}>
+                   {heart}
                </button>
            </div>
-        
-        
-        
-        
-        
-        
-        </div>
-      
-  
-  
-  
-  
-  
-  
-  
-  
-  
+        </div> 
   </div>)
 
 
