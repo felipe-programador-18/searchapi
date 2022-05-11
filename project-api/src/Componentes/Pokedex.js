653504457c98cb025a61013388881295f1a3a46d
Felipe <machadofelipe2016@outlook.com>
2022-05-11 17:46:16 -0300

    c20-coding and see solution about pokedex

--- project-api/src/Componentes/Pokedex.js
+++ project-api/src/Componentes/Pokedex.js
@@ -21,15 +21,18 @@ const Pokedex = (props) =>{
 
   return( <div>
             <div className='pokedex-header'>
-              <h1>Pokedex:😎</h1>
-              <Pagination/>
+                <h1>Pokedex:😎</h1>
+                <Pagination/>
 
               <div>
-               {loading ? (<div>Loading Page ....</div>):
-                  <div>
-                         
-
-                  </div>   }
+               {loading ? (<div>Loading Page ....</div> ):
+                  (<div className='pokedex-grid'>
+                  {pokemons && pokemons.map((pokemon, index) =>{
+                      return(<Pokemon key={index} pokemon={pokemon}/>
+                      );
+                  })}
+                  </div>
+                   )}
               </div>
 
             </div>
