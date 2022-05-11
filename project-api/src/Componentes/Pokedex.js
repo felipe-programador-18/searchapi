ff5b0d3fa2281bbc1ed6b184738bf46ad6a77b5c
Felipe <machadofelipe2016@outlook.com>
2022-05-11 17:10:55 -0300

    c18-make left and right handling

--- project-api/src/Componentes/Pokedex.js
+++ project-api/src/Componentes/Pokedex.js
@@ -4,8 +4,20 @@ import Pagination from './Pagination'
 
 const Pokedex = (props) =>{
   const {pokemons, page, loading, setPage, TotalPage} = props
-
-
+   
+  // left to stay in page!!!
+  const onLeftHaling = () =>{
+    if(page> 0){
+     setPage(page-1)
+    }
+  }
+  
+  const onRightHaling = () =>{
+   if(page+1 !== TotalPage){
+       setPage(page+1)
+   }
+  }
+ 
 
   return(
 
