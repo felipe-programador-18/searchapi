a511891f0512ee4a85d15d2ff62a23857c493f0f
Felipe <machadofelipe2016@outlook.com>
2022-05-11 17:05:21 -0300

    c17-put in hands in pokedex

--- project-api/src/Componentes/Pokedex.js
+++ project-api/src/Componentes/Pokedex.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Pokemon from './Pokemon'
+import Pagination from './Pagination'
 
 const Pokedex = (props) =>{
   const {pokemons, page, loading, setPage, TotalPage} = props
