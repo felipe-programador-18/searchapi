7c5d7a0aa2088e2fb93d76006c5604db46f59b44
Felipe <machadofelipe2016@outlook.com>
2022-05-11 17:58:06 -0300

    c22-when make props remember i can get in inside another code

--- project-api/src/Componentes/Pagination.js
+++ project-api/src/Componentes/Pagination.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
-
 const Pagination =(props) => {
-    return()
+    // this way am get all state with props
+    const{page,TotalPage, onLeftClick, onRightClick } = props
+    
+    return(<div>
+        <button onClick={onLeftClick} >⬅</button>
+        <div>{page}: {TotalPage}</div>
+        <button onClick={onRightClick}> ➡</button>
+    </div>)
 
 }
 
